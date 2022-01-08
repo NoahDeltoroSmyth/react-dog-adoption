5a68f3a91ea8588ea45160b3df679fe50b96641e
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-01-07 17:08:57 -0800

    DogAdmin header test

--- src/views/DogAdmin.test.js
+++ src/views/DogAdmin.test.js
@@ -1,16 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import DogList from './DogList';
+import DogAdmin from './DogAdmin';
 
-it('should render a list of dogs', async () => {
+it('should render h1 text', async () => {
   //render
   const { container } = render(
     <MemoryRouter>
-      <DogList />
+      <DogAdmin />
     </MemoryRouter>
   );
-  //load
-  await screen.findByText('Dixie');
-  //snapshot
+  //wait for load
+  await screen.findByText('Dog Information');
+  //take screen shot
   expect(container).toMatchSnapshot();
 });
