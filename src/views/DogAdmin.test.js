2013b580a6255aab9048e892133adbf7e9e00a04
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-02-08 12:10:13 -0800

    fixiing test

--- src/views/DogAdmin.test.js
+++ src/views/DogAdmin.test.js
@@ -1,12 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+import { DogProvider } from '../Context/DogContext';
 import DogAdmin from './DogAdmin';
 
 it('should render h1 text', async () => {
   //render
   const { container } = render(
     <MemoryRouter>
-      <DogAdmin />
+      <DogProvider>
+        <DogAdmin />
+      </DogProvider>
     </MemoryRouter>
   );
   //wait for load
